2d950c89dd0431b47b6f8c2cc56ba8cb28bdb3b5
Just-a-NoobieDev <carljames.roxas.m@gmail.com>
2022-07-31 04:39:38 +0800

    Add backend api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500/";
+const BASE_URL = "https://backend-mern-api.herokuapp.com/";
 
 export default axios.create({
   baseURL: BASE_URL,
